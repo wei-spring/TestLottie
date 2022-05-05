0011ac4e464c2353cb1e3f3ac6e1f2f341e64bd3
weichunsheng <weichunsheng@jd.com>
2022-05-05 14:07:47 +0800

    Initial commit

---
+++ app/src/main/java/com/jd/jxsaas/testlottie/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jd.jxsaas.testlottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        LottieAnimationView lottieAnimView = findViewById(R.id.mLottieAnimationView);
+//        lottieAnimView.setImageAssetsFolder("lottie/comm/images");
+//        lottieAnimView.setAnimation("lottie/comm/loading.json");
+    }
+}
